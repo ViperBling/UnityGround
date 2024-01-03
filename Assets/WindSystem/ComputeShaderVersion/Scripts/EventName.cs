85d2552847f8ab2335b34af7003c435d350992ad
ViperBling <qiusongtao2010@hotmail.com>
2024-01-03 23:34:02 +0800

    [M] Rollback to URP

--- Assets/WindSystem/ComputeShaderVersion/Scripts/EventName.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class EventName
-{
-    public const string CreateWindMotor = "CreateWindMotor";
-    public const string DeleteWindMotor = "DeleteWindMotor";
-    public const string UpdateWinWindMotor = "UpdateWinWindMotor";
-    public const string UpdateWinWindMotorDetail = "UpdateWinWindMotorDetail";
-    public const string UpdateHandlerIcon = "UpdateHandlerIcon";
-    public const string DeleteWindHandler = "DeleteWindHandler";
-}
